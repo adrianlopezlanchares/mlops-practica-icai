f4f6c43d13f89f565958e1b3e50a2de274074dac
Adrian <coco@Adrians-MacBook-Air.local>
2025-09-26 10:47:17 +0200

    chage dagshub access

--- train.py
+++ train.py
@@ -2,13 +2,13 @@ import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
 
-import dagshub
-dagshub.init(repo_owner='adrianlopezlanchares', repo_name='mlops-practica-icai', mlflow=True)
+tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(tracking_uri)
 
 # Cargar el conjunto de datos desde el archivo CSV
 try:
@@ -48,3 +48,15 @@ mlflow.log_param("n_estimators", n_estimators)
 mlflow.log_metric("accuracy", accuracy)
 print(f"Modelo entrenado y precisión: {accuracy:.4f}")
 print("Experimento registrado con MLflow.")
+
+# --- Sección de Reporte para CML ---
+# 1. Generar la matriz de confusión
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.title('Matriz de Confusión')
+plt.xlabel('Predicciones')
+plt.ylabel('Valores Reales')
+plt.savefig('confusion_matrix.png')
+print("Matriz de confusión guardada como 'confusion_matrix.png'")
+# --- Fin de la sección de Reporte ---
