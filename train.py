52ae9d8e5f908f876c078e3869e8d22df4d5911b
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-12-21 01:17:45 +0100

    fix name

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ accuracy = accuracy_score(y_test, y_pred)
 
 # Registrar el modelo con MLflow
 mlflow.sklearn.log_model(
-    model, name="random-forest-model", registered_model_name="iris-rf"
+    model, artifact_path="random-forest-model", registered_model_name="iris-rf"
 )
 
 # Registrar parámetros y métricas
